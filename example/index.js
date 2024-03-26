5a4f44eac70c9ee00a3d8077fdfb4d62900a4637
abdel-17 <abdelrahmanb015@gmail.com>
2024-03-26 03:31:53 +0200

    fix example

--- example/index.js
+++ example/index.js
@@ -4,6 +4,10 @@ document.addEventListener("DOMContentLoaded", () => {
 	const ripple = new Ripple(document.getElementById("ripple"));
 	ripple.attach();
 
+	document.getElementById("easing").addEventListener("change", function () {
+		ripple.easing = this.value;
+	});
+
 	document.getElementById("toggle").addEventListener("change", function () {
 		ripple.disabled = this.checked;
 	});
